929273caea29488ca8b60f1cc452f58207f02be5
RobsonDev <robson.arcoleze29@gmail.com>
2022-11-17 13:56:49 -0300

    Project Created - Subindo backend

---
+++ backend/src/test/java/com/robsonArcoleze/dsmeta/DsmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robsonArcoleze.dsmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
